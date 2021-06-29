75c5aeef183bf8dee29b44f06da7d07f3f0c80c4
Ahmad Jallad <ahmadjlad@hotmail.com>
2021-06-29 14:47:57 +0300

    push change to the correct links for a

--- script.js
+++ script.js
@@ -1,5 +1,99 @@
-// this is new file
-for (let i = 0; i < 5; i++) {
-    console.log(i);
-    
-}
+'use strict'
+/*
+// // first challenge
+// let title_forElzero = "Elzero", description_var = "Elzero Wep School", chalengedate = "25/10";
+// let card_continuer = `<div>
+//   <h3>${title_forElzero}</h3>
+//   <p>${description_var}</p>
+//   <span>${chalengedate}</span>
+// </div>`;
+// // document.write(card_continuer.repeat(4))
+// console.log(card_continuer.repeat(4));
+*/
+
+// unary plus 
+// console.log(+"100");
+
+/*
+
+// Type Coercion || Type casting
+
+let a = "10", b = 20, c = true;
+console.log(a + b);
+console.log(+a + b);
+console.log(a - b);
+console.log(false - c);
+console.log(a + b + c);
+console.log(a + b + c);
+console.log(+a + b + c);
+/*/
+
+// second challenge
+
+/*
+let a = 10;
+let b = "20";
+let c = 80;
+let d = "-100";
+let e = "20";
+let f = 30;
+let g = true;
+console.log(-d * +e);//2 000
+console.log(-d + +((e) += (e)) + f + (g = ++g));// 173
+console.log((++(e) * ++g) + (- --d) + f)
+*/
+
+// numbers
+
+/*
+console.log(100..toString())
+console.log((100).toString())
+console.log((1000.555).toFixed())
+console.log(Number("100 ahmad"), 'Number("100 ahmad")');
+parseInt search for number
+console.log(parseInt("100 ahmad"));
+console.log(parseInt("ahmad 100 jallad"));
+console.log(parseFloat("100.55"), "100.5 passed");
+console.log(Number.isInteger(100), "100 passed");
+console.log(Number.isInteger(100.5), "100.5 passed");
+console.log(Number.isNaN(100.5 / "ahmad"), '100.5 / "ahmad"');
+console.log(Number.isNaN("100 ahmad"), "ahmad");
+*/
+
+// math
+
+/*
+// let testArr = [3, 5, 10, 55, 8, 7, 9, 6, 54, 23, 21];
+// console.log(Math.round(99.5), "Math.round(99.5)")
+// console.log(Math.ceil(99.2), "Math.ceil(99.2), بقرب اي عشر")
+// console.log(Math.floor(99.9), "Math.floor(99.9), floor it")
+// console.log(Math.min(3, 5, 10, 55, 8, 7, 9, 6, 54, 23, 21), "3, 5, 10, 55, 8, 7, 9, 6, 54, 23, 21")
+// console.log(Math.max(3, 5, 10, 55, 8, 7, 9, 6, 54, 23, 21), "3, 5, 10, 55, 8, 7, 9, 6, 54, 23, 21")
+// console.log(Math.pow(2, 4), "Math.pow(2, 4) same as 2 ** 4")
+// console.log(Math.random() * 2, "Math.random()")
+// console.log(Math.trunc(99.5), "Math.trunc(99.5) cut the flout point")
+// console.log(testArr)
+*/
+
+
+// challenge 
+
+/*
+let a = 1_00, b = 2_00.5, c = 1e2, d = 2.4;
+
+// min , parstint
+console.log(Math.round(Math.min(a, b, c, d)));
+//   10_000 a + b only one time
+console.log(Math.pow(c, Math.floor(d)));
+// 2 from 4 methods
+console.log(Math.floor(d));
+console.log(Math.round(d));
+console.log(Math.trunc(d));
+console.log();
+
+// b + d 66.67 as string , 67 as number
+console.log(b / 3)
+*/
+
+let a = ".firstChild .lastChild"
+console.log(a.toLowerCase());
